22f99f9c7aab7e73543cd31d79dac872455934fb
asbaden <gusbaden96@gmail.com>
2021-02-04 14:17:05 -0800

    completed merge two sorted lists

--- restOfMyDays.py
+++ restOfMyDays.py
@@ -138,3 +138,26 @@ class Solution:
                 return i
 
         return -1
+
+# Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        #create a dummy node for output
+        dummy = ListNode()
+        tail = dummy 
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
+                l1 = l1.next 
+            else: 
+                tail.next = l2
+                l2 = l2.next
+            # tail is updated regardless
+            tail = tail.next 
+        #case for empty list
+        if l1:
+            tail.next = l1
+        elif l2:
+            tail.next = l2
+        return dummy.next
