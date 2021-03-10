52566894f2ab277a58db6b3aaccb84ebb9aa4983
asbaden <gusbaden96@gmail.com>
2021-03-09 20:45:56 -0800

    completed statues problem

--- restOfMyDays.py
+++ restOfMyDays.py
@@ -226,3 +226,6 @@ def shapeArea(n):
     #if n is 1 return 1 
     #else if n > 1 return (n*2)*(n-1)+1 
     return (n*2)*(n-1)+1
+# Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+def makeArrayConsecutive2(statues):
+    return (max(statues) - min(statues)) - len(statues) + 1
